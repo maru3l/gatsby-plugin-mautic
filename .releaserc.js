e93c57341f4831cfe3badba2c5b04b17b35ea92e
Marc-Antoine Ruel <hello@marcantoineruel.com>
2020-04-15 15:46:24 -0400

    :tada: First commit, second attempt

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,3 @@
-const { promisify } = require('util')
-const dateFormat = require('dateformat')
-const readFileAsync = promisify(require('fs').readFile)
-
-// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
-
-// the *.hbs template and partials should be passed as strings of contents
-const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
-const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR, 'commit-template.hbs'))
-
 module.exports = {
   branches: ["master"],
   plugins: [
@@ -21,20 +11,6 @@ module.exports = {
             ':ambulance:',
             ':lock:'
           ]
-        },
-        releaseNotes: {
-          template,
-          partials: { commitTemplate },
-          helpers: {
-            datetime: function (format = 'UTC:yyyy-mm-dd') {
-              return dateFormat(new Date(), format)
-            }
-          },
-          issueResolution: {
-            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
-            baseUrl: 'https://github.com',
-            source: 'github.com'
-          }
         }
       }
     ],
